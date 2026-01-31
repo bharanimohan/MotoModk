3179a4c50efb2af60c8010823c31a61073ae0d96
Bharani <bharanimass5@gmail.com>
2026-01-31 19:07:18 +0530

    DB

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=shop_db';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'pgsql:host=dpg-d5v08nbuibrs73cfcln0-a;dbname=shop_db_ci7z';
+$user_name = 'shop_db_ci7z_user';
+$user_password = 'p1ybapG9V0einLIRLkK527qsHo8rwwR3';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
